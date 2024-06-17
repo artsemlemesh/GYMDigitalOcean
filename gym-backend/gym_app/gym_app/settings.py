289394206b4b2ff2f8a0dbad4bee5fbce4e955b3
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-17 14:55:38 +0800

    d

--- gym-backend/gym_app/gym_app/settings.py
+++ gym-backend/gym_app/gym_app/settings.py
@@ -110,22 +110,22 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": '',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '',  
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'my_db',
+        'USER': 'jerry',
+        'PASSWORD': 'postgresql',
+        'HOST': 'localhost',
+        'PORT': '5432',  
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
